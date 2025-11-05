14057986edbc5de7d143e5c7dc135998a606fa7c
TkTJP <tktjpop@gmail.com>
2025-11-05 12:20:31 +0900

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,48 +1,38 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>マイページ</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>マイページ</title>
+  <link rel="stylesheet" href="css/profile.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
-    <!-- ヘッダー -->
-    <div class="header">
-        <div class="logo">
-            <img src="logo.png" alt="SATONOMI ロゴ">
-            <span>SATONOMI</span>
-        </div>
-        <div class="icons">
-            <i class="fa-solid fa-cart-shopping"></i>
-            <i class="fa-solid fa-user"></i>
-        </div>
-    </div>
 
-    <!-- タイトル -->
+  <?php include('header.php'); ?>
+
+  <div class="mypage-container">
     <div class="mypage-header">
-        マイページ
+      マイページ
     </div>
 
-    <!-- プロフィール -->
     <div class="profile">
-        <img src="https://via.placeholder.com/100" alt="プロフィール画像">
-        <div class="name">里野　美里</div>
-        <div class="points">所持ポイント：1500</div>
+      <img src="https://via.placeholder.com/100" alt="プロフィール画像">
+      <div class="name">里野　美里</div>
+      <div class="points">所持ポイント：1500</div>
     </div>
 
-    <!-- メニュー -->
     <div class="menu">
-        <button><i class="fa-solid fa-user"></i>My情報 <i class="fa-solid fa-angle-right"></i></button>
-        <button><i class="fa-solid fa-clock"></i>購入履歴 <i class="fa-solid fa-angle-right"></i></button>
-        <button><i class="fa-solid fa-star"></i>サブスクに登録する <i class="fa-solid fa-angle-right"></i></button>
-        <button><i class="fa-solid fa-face-smile"></i>スタンプカード <i class="fa-solid fa-angle-right"></i></button>
+      <button><i class="fa-solid fa-user"></i>My情報 <i class="fa-solid fa-angle-right"></i></button>
+      <button><i class="fa-solid fa-clock"></i>購入履歴 <i class="fa-solid fa-angle-right"></i></button>
+      <button><i class="fa-solid fa-star"></i>サブスクに登録する <i class="fa-solid fa-angle-right"></i></button>
+      <button><i class="fa-solid fa-face-smile"></i>スタンプカード <i class="fa-solid fa-angle-right"></i></button>
     </div>
 
-    <!-- ログアウト -->
     <div class="logout">
-        ログアウト
+      ログアウト
     </div>
+  </div>
+
 </body>
 </html>
