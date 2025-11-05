b2b1e9b89f05e537251f00b71cc36c632ae743db
TkTJP <tktjpop@gmail.com>
2025-11-05 12:20:24 +0900

    Update header.php

--- header.php
+++ header.php
@@ -1,27 +1,12 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>header</title>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-  <style>
-  </style>
-</head>
-<body>
-  <header>
-    <div class="logo">
-      <img src="img/logo.png" alt="ロゴ">
-      <span class="site-title">SATONOMI</span>
+<header class="site-header">
+  <div class="header-inner">
+    <div class="header-left">
+      <img src="logo.png" alt="SATONOMI ロゴ" class="header-logo">
+      <span class="header-title">SATONOMI</span>
     </div>
-    <div class="level-right">
-      <a class="icon-circle mr-4" href="#">
-        <i class="fas fa-shopping-cart"></i>
-      </a>
-      <a class="icon-circle" href="#">
-        <i class="fas fa-user"></i>
-      </a>
+    <div class="header-icons">
+      <a href="cart.php" class="icon-link"><i class="fa-solid fa-cart-shopping"></i></a>
+      <a href="profile.php" class="icon-link"><i class="fa-solid fa-user"></i></a>
     </div>
-  </header>
-</body>
-</html>
+  </div>
+</header>
