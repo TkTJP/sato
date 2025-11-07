1df945e41cea25225063217aa99788575eee52f5
TkTJP <tktjpop@gmail.com>
2025-11-07 11:43:36 +0900

    Update header.php

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 <header class="site-header">
   <div class="header-inner">
     <div class="header-left">
