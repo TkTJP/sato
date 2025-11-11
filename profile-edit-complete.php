3018c4bf46a097c8250af206100c90c12aee9a08
isisis210 <otanekonekorugasaburo1818@gmail.com>
2025-11-11 09:49:18 +0900

    Create profile-edit-complete.php

---
+++ profile-edit-complete.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>プロフィール編集完了</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php include('header.php'); ?>
+
+    <div class="app-container">
+        <main class="completion-container">
+            <div class="message-area">
+                <p class="completion-message">プロフィールの更新が完了しました！</p>
+            </div>
+
+            <!-- マイページに戻る -->
+            <div class="button-area">
+                <a href="profile.php" class="top-button">マイページへ戻る</a>
+            </div>
+        </main>
+    </div>
+</body>
+</html>
