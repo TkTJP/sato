5601ae9431771705295c2a46cf759c0f8fffe162
isisis210 <otanekonekorugasaburo1818@gmail.com>
2025-11-11 10:21:13 +0900

    Update profile-edit-complete.php

--- profile-edit-complete.php
+++ profile-edit-complete.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>プロフィール編集完了</title>
+    <title>My情報編集完了</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -12,7 +12,7 @@
     <div class="app-container">
         <main class="completion-container">
             <div class="message-area">
-                <p class="completion-message">プロフィールの更新が完了しました！</p>
+                <p class="completion-message">My情報の更新が完了しました！</p>
             </div>
 
             <!-- マイページに戻る -->
