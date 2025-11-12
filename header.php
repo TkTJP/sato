d5e2d43956b8d4a95d97f4faf5c8db42f76aeff6
ayumu-2401173 <2401173@s.asojuku.ac.jp>
2025-11-12 10:28:16 +0900

    pull

--- header.php
+++ header.php
@@ -1,10 +1,13 @@
 <link rel="stylesheet" href="style.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
 <header class="site-header">
   <div class="header-inner">
     <div class="header-left">
-      <img src="img/logo.png" alt="SATONOMI ロゴ" class="header-logo">
-      <span class="header-title">SATONOMI</span>
+      <a href="top.php" class="header-logo-link">
+        <img src="img/logo.png" alt="SATONOMI ロゴ" class="header-logo">
+        <span class="header-title">SATONOMI</span>
+      </a>
     </div>
     <div class="header-icons">
       <a href="cart-confirm.php" class="icon-link"><i class="fa-solid fa-cart-shopping"></i></a>
