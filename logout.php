32412edc0f7af08c7630a50da1db149eaf985fa6
TkTJP <tktjpop@gmail.com>
2025-11-12 10:51:27 +0900

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,25 +1,7 @@
 <?php
 session_start();
-require_once('db-connect.php');
 
-if (isset($_SESSION['customer_id'])) {
-    try {
-        $pdo = new PDO($connect, USER, PASS);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        // DB側のトークンも削除
-        $sql = "UPDATE customers SET remember_token = NULL WHERE customer_id = ?";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([$_SESSION['customer_id']]);
-    } catch (PDOException $e) {
-        // 無視
-    }
-}
-
-// Cookie削除
-setcookie('remember_token', '', time() - 3600, '/');
-
-// セッション終了
+// セッションを破棄
 $_SESSION = [];
 session_destroy();
 
