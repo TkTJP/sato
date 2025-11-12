fcd9eb5f07b407b21e3fec3de79c1a4b50b4c269
TkTJP <tktjpop@gmail.com>
2025-11-12 11:54:15 +0900

    Update login.php

--- login.php
+++ login.php
@@ -66,8 +66,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ログイン</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <title>ログイン</title>
 </head>
 <body>
 
