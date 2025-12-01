206089ca95f5109fa69f37748d35db5bed363c87
TkTJP <tktjpop@gmail.com>
2025-12-01 10:15:41 +0900

    Update product_detail.php

--- product_detail.php
+++ product_detail.php
@@ -37,6 +37,7 @@ if ($customer_id) {
 <head>
 <meta charset="UTF-8">
 <link rel="stylesheet" href="style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?= htmlspecialchars($product['name']); ?></title>
 </head>
 <body>
