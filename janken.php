5a324cefb4ec1ed8536e5262461896d0e05e1aa6
TkTJP <tktjpop@gmail.com>
2025-12-03 22:38:28 +0900

    Update janken.php

--- janken.php
+++ janken.php
@@ -91,6 +91,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html lang="ja">
 <head>
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>じゃんけん</title>
 
 <style>
